769aa9c5691dfd300dce55adb4e34ad478f23bef
Linnea <linneaj95@hotmail.com>
2019-01-17 15:24:43 +0100

    See if picked cards are the same

--- main.js
+++ main.js
@@ -15,34 +15,55 @@ var memoryCards = [
   { back: backPic, name: "Lobster", id: 6}
 ]
 
+
 var start = document.getElementById("start-game").addEventListener("click", function(e) {
+  e.preventDefault();
+  // hide the landing page
   var startDiv = document.getElementById("start-div")
   startDiv.setAttribute("class", "hidden");
-
+  // show the score counting div
+  document.getElementById("score-counter").setAttribute("class", "")
+  // show the cards on the board
   createCards();
-  chooseCards();
 
-
-})
-
-
-// show the front of the card when it is clicked
-function chooseCards() {
-  var openCards = 0;
+  // the cards with the frontpage visible
+  var choosenCards = [];
+  var foundPairs = [];
   var cards = document.getElementsByTagName("li");
   for (var j = 0; j < cards.length; j++) {
     cards[j].addEventListener("click", function(e) {
       e.preventDefault();
-      openCards++;
+      // show the front of the card when it is clicked
       if (this.className == "") {
         this.setAttribute("class", "active")
       }
-      else {
-        this.setAttribute("class", "")
+      choosenCards += this.id;
+      if (choosenCards.length == 2) {
+        if (choosenCards[0] == choosenCards[1]) {
+          var x = memoryCards.findIndex(x => x.id == choosenCards[0])
+          memoryCards.splice(x, 2)
+          foundPairs += memoryCards[x] + memoryCards[x+1];
+          console.log(foundPairs)
+          console.log(memoryCards)
+        }
+        // wait two seconds before turning the cards back around
+        setTimeout(function triggerCardTurn() {
+          turnDownCards()
+        }, 2000);
+        choosenCards = [];
+        
+        // shows the back of the cards again, if there is no match
+        function turnDownCards() {
+          var cardsToTurn = document.getElementsByClassName("active");
+          for (var i = 0; i < cardsToTurn.length; i + 2) {
+            cardsToTurn[i].setAttribute("class", "")
+          }
+        }
       }
     });
   }
-}
+})
+
 
 
 
@@ -57,6 +78,7 @@ function createCards() {
     var cardTitle = document.createElement("h2");
     cardTitle.innerHTML = memoryCards[i].name;
     cardTitle.setAttribute("class", memoryCards[i].name)
+    card.setAttribute("id", memoryCards[i].id)
 
     card.appendChild(cardTitle);
     card.appendChild(backOfCard);
